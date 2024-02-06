8c1ec69f34134b21eb15fb2524d9320d84216bfd
Vladous <forman.vl@gmail.com>
2024-02-06 16:52:47 +0100

    Formátování users-cshtml

---
+++ Models/EditUserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZaverecnyProjektForman2.Models
+{
+    public class EditUserViewModel
+    {
+        public string UserId { get; set; } = "";
+        [Display(Name = "Uživatelské jméno")]
+        public string UserName { get; set; } = "";
+        public string Email { get; set; } = "";
+        [Display(Name = "Heslo")]
+        public string Password { get; set; } = "";
+        [Display(Name = "Datum registrace")]
+        public DateTime? RegistrationDate { get; set; } = null;
+        [Display(Name = "Práva")]
+        public string Role { get; set; } = "";
+
+    }
+}
